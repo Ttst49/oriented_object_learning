a72e61c3200f371d6479df88d775322ab5077fd4
Ttst <thibautstachnick@gmail.com>
2024-04-14 14:35:45 +0200

    add all methods for AverageCollection structure

--- src/lib.rs
+++ src/lib.rs
@@ -9,4 +9,24 @@ impl AverageCollection {
         self.updateAverage();
     }
 
+    pub fn remove(&mut self,)->Option<i64>{
+        let result = self.list.pop();
+        match result {
+            None => None,
+            Some(value) => {
+                self.updateAverage();
+                Some(value)
+            }
+        }
+    }
+
+    pub fn average(&self)->f64{
+        self.average
+    }
+
+    pub fn updateAverage(&mut self){
+        let total = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+
 }
